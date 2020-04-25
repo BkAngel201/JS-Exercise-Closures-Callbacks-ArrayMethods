3b2e61371c0233c9fcc98a8a7ad1601a820ba85f
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-04-24 21:48:11 -0400

    Challenge `stringSmash` done

--- index.js
+++ index.js
@@ -224,8 +224,12 @@ function removeApple(strings) {
  * 
  * [2] Invoking `stringSmash` with `['a', 'b', 'c' ]` will return `abc`.
 */
-function stringSmash(/* code here */) {
-  /* code here */
+function stringSmash(strings) {
+
+  let output = strings.reduce((total, element) => {
+    return total + element;
+  });
+  return output;
 }
 
 // A local community center is holding a fund raising 5k fun run and has invited
