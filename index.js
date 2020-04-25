c309b33f1a02978715b57a6e1dc9e5c5d5cd4556
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-04-24 21:53:12 -0400

    Challenge `firstNamesAllCaps` done

--- index.js
+++ index.js
@@ -266,8 +266,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let output = runners.map(element => {
+    return element.first_name.toUpperCase();
+  });
+  return output;
 }
 
 /**
